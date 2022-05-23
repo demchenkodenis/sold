4b8460ecd4fb1682b32e56e5503df97fba17f1da
demchenkodenis <denisdemchenko@icloud.com>
2022-05-23 23:48:13 +0300

    доделать еще третий тест, если второй работает правильно

---
+++ testing.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost:8889
+-- Время создания: Май 23 2022 г., 23:45
+-- Версия сервера: 5.5.42
+-- Версия PHP: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- База данных: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `test_1`
+--
+
+CREATE TABLE `test_1` (
+  `id` int(11) NOT NULL,
+  `answer_1` text NOT NULL,
+  `answer_2` text NOT NULL,
+  `answer_3` text NOT NULL,
+  `answer_4` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `test_4`
+--
+
+CREATE TABLE `test_4` (
+  `id` int(11) NOT NULL,
+  `answer_1` varchar(255) NOT NULL,
+  `answer_2` varchar(255) NOT NULL,
+  `answer_3` varchar(255) NOT NULL,
+  `answer_4` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `test_1`
+--
+ALTER TABLE `test_1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `test_4`
+--
+ALTER TABLE `test_4`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `test_1`
+--
+ALTER TABLE `test_1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `test_4`
+--
+ALTER TABLE `test_4`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
